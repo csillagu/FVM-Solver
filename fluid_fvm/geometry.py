1c2efcdbd8b74df30c33265aaed7ac87ab131359
Ujvari Andras <ujvariandris@gmail.com>
2024-05-12 00:34:09 +0200

    Added line numbering function to Assembly

--- fluid_fvm/geometry.py
+++ fluid_fvm/geometry.py
@@ -7,12 +7,31 @@ class Assembly():
         self.base_polygon.plot(ax, "white")
         for p in self.polygon_list:
             p.plot(ax)
-        
+    
+
+    def assemble(self):
+    #Assembles the Assembly, assigns names to lines, points and polygons
+        self._namePolygons()
+        self._nameLines()
+
+
+    def _namePolygons(self):
+        self.base_polygon.setName("Polygon_0")
+        for p in range(len(self.polygon_list)):
+            self.polygon_list[p].setName("Polygon_"+str(p+1))
+    
+    def _nameLines(self):
+        previous_names, previous_line_num = self.base_polygon.setLineNames(0, [])
+        #previous_line_num = len(previous_names)
+        for p in range(len(self.polygon_list)):
+            newlines,previous_line_num = self.polygon_list[p].setLineNames(previous_line_num, previous_names) 
+            previous_names=previous_names+newlines
 
 class Polygon():
     def __init__(self, point_list) -> None:
         self.points = point_list
-        
+        self.lines = [Line(self.points[i], self.points[(i+1)%len(self.points)]) for i, p in enumerate(self.points)]
+
     def getPointsXY(self):
         points_y = [point.y for point in self.points]
         points_x = [point.x for point in self.points]
@@ -22,16 +41,48 @@ class Polygon():
         points_x, points_y = self.getPointsXY()
 
         ax.fill(points_x, points_y, color = color, edgecolor="black")
-        pass
+    
+    def setName(self, name):
+        self.name = name
+        
+    def lineAlreadyExists(line, lines):
+        if len(lines) == 0:
+            return False, ""
+        for lin in lines:
+            if lin == line:
+                return True, lin.name
+        return False, ""
+    
+    def setLineNames(self,start_index, previousLines):
+        next_name = start_index
+        for p in range(len(self.lines)):
+            ex, name = Polygon.lineAlreadyExists(self.lines[p], previousLines)
+            if ex:
+                self.lines[p].setName(name)
+            else:
+                self.lines[p].setName("Line_"+str(next_name))
+                next_name+=1
+        return self.lines, next_name
+
+
 
 class Line():
     def __init__(self, p1, p2) -> None:
         assert(p1!=p2)
         self.p1 = p1
         self.p2 = p2
+        self.name = ""
     
     def plot(self, ax, fmt="b-"):
         ax.plot([self.p1.x, self.p2.x], [self.p1.y, self.p2.y])
+        ax.text((self.p1.x+self.p2.x)/2, (self.p1.y+self.p2.y)/2, self.name)
+    
+    def setName(self, name):
+        self.name=name
+    
+    def __eq__(self, other: object) -> bool:
+        
+        return (self.p1 == other.p1) and (self.p2 == other.p2)
 
 
 class Point():
@@ -44,3 +95,7 @@ class Point():
     
     def plot(self, ax, fmt = "bx"):
         ax.plot(self.x, self.y,fmt)
+
+    def __eq__(self, other: object) -> bool:
+        return (self.x == other.x) and (self.y == other.y)
+
