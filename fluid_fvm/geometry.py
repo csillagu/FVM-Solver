92e6936ed09f40568144c627f5c5fc7b4c66ba24
Ujvari Andras <ujvariandris@gmail.com>
2024-05-11 20:19:55 +0200

    Added plot functions to the geometries

--- fluid_fvm/geometry.py
+++ fluid_fvm/geometry.py
@@ -5,12 +5,20 @@ class Assembly():
 class Polygon():
     def __init__(self, line_list) -> None:
         self.lines = line_list
+        self.points_x = [line.p1.x for line in line_list]
+        self.points_y = [line.p1.y for line in line_list]
+        
+    def plot(self, ax, fmt="b"):
+        pass
 
 class Line():
     def __init__(self, p1, p2) -> None:
         assert(p1!=p2)
         self.p1 = p1
         self.p2 = p2
+    
+    def plot(self, ax, fmt="b-"):
+        ax.plot([self.p1.x, self.p2.x], [self.p1.y, self.p2.y])
 
 
 class Point():
@@ -20,3 +28,6 @@ class Point():
 
     def getPosition(self):
         return (self.x, self.y)
+    
+    def plot(self, ax, fmt = "bx"):
+        ax.plot(self.x, self.y,fmt)
