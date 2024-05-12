6533ac618316668357608ef634a8ca588341f7bc
Ujvari Andras <ujvariandris@gmail.com>
2024-05-12 22:35:02 +0200

    Added assign material function to Component

--- fluid_fvm/project.py
+++ fluid_fvm/project.py
@@ -2,15 +2,26 @@
 class Component():
 
     def __init__(self) -> None:
-        pass
+        self.materials = []
+        self.material_connectivity ={}
 
     def setAssembly(self, a):
         self.assembly = a
 
+    def assignMaterial(self, name, material):
+        if material.name not in [mat.name for mat in self.materials]:
+            self.materials.append(material)
+        self.material_connectivity[name] = material.name
+    
+
 
 class Material():
-    def __init__(self, **kvargs) -> None:
+    def __init__(self, name, **kvargs) -> None:
         self.properties = kvargs
-    
+        self.name = name
+
     def getProperty(self, name):
-        return self.properties[name]
+        if name in self.properties:
+            return self.properties[name]
+        else:
+            return None
