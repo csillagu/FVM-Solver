bb6828d4ba1a9a8e1ae557d41cd6228e17809ee6
Ujvari Andras <ujvariandris@gmail.com>
2024-05-12 20:13:28 +0200

    Added Material class

--- fluid_fvm/project.py
+++ fluid_fvm/project.py
@@ -7,4 +7,10 @@ class Component():
     def setAssembly(self, a):
         self.assembly = a
 
-        
+
+class Material():
+    def __init__(self, **kvargs) -> None:
+        self.properties = kvargs
+    
+    def getProperty(self, name):
+        return self.properties[name]
