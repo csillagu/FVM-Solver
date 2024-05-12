638b606d60c91a9fb24740abf8a526dac64c2d8d
Ujvari Andras <ujvariandris@gmail.com>
2024-05-12 23:14:46 +0200

    Added Component plot function and material color property

--- fluid_fvm/project.py
+++ fluid_fvm/project.py
@@ -13,12 +13,25 @@ class Component():
             self.materials.append(material)
         self.material_connectivity[name] = material.name
     
+    def plot(self, ax):
+        colorMap = dict( (p_name,  self._findMaterialByName( self.material_connectivity[p_name]).getProperty("color")) for p_name in self.material_connectivity )
+        #Poly1 : mat1.name, poly2_mat2.name
+        print(colorMap)
+        self.assembly.plot(ax,colorMap=colorMap)
+
+    def _findMaterialByName(self, name):
+        for mat in self.materials:
+            if mat.name == name:
+                return mat
+        return None
 
 
 class Material():
     def __init__(self, name, **kvargs) -> None:
         self.properties = kvargs
         self.name = name
+        if self.getProperty("color") is None:
+            self.properties["color"] = "#c8ffc8"
 
     def getProperty(self, name):
         if name in self.properties:
