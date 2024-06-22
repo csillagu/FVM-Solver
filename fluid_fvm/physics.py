03775fb6deeb3d712b35378022c05a4e2ca2870b
Ujvari Andras <ujvariandris@gmail.com>
2024-06-22 17:08:32 +0200

    Added inner flux calculation with example and renamed the example file to thermal

--- fluid_fvm/physics.py
+++ fluid_fvm/physics.py
@@ -1,7 +1,10 @@
+import numpy as np
+import fluid_fvm.geometry as geo
+
 class Physics():
     pass
 
-class Couette(Physics):
+class HeatTransfer(Physics):
     def __init__(self, assembly, boundaries) -> None:
         super().__init__()
         self.assembly = assembly
@@ -11,6 +14,50 @@ class Couette(Physics):
         assert(set(bcdict.keys()).issubset(self.assembly.getLineNames()))
         assert(set(bcdict.keys()).issuperset(self.assembly.getBaseLineNames()))
         self.boundaries = bcdict
+    def isBoundary(self, name):
+        return name in self.boundaries.keys()
     
 
-        
+    def getFluxInner(self, material, volume, face_normal,  neighbour_vector):
+        gamma = material.getProperty("gamma")
+
+
+
+        Fe, Fc = self._Gradient(neighbour_vector=neighbour_vector)
+        # J = gamma * dot(grad(phi), Se) =gamma*{ (Fc*phi_c+Fe*phi_e).x*Se.x + (Fc*phi_c+Fe*phi_e).y*Se.y } = gamma*{  Jc*phi_c + Je*phi_e } 
+
+        Jc = Fc*face_normal
+        Je = Fe*face_normal
+
+        coeff_mid = gamma*Jc
+        coeff_neighbour = gamma*Je
+        coeff_const = 0
+
+        return coeff_mid, coeff_neighbour, coeff_const
+    
+    def _Gradient(self, neighbour_vector):
+        # grad(phi) = Vector(  (phi_e-phi_c)/diff_x, (phi_e-phi_c)/diff_y ) =  Fc*phi_c+Fe*phi_e  
+        # (diffx and y are the x and y components of the vector pointing from the current to the neighbouring face)
+        # If diff_x or y is 0 the respective component of the gradient is zero
+        if neighbour_vector.x == 0:
+            diff_x = 0.1
+            corr_x = 0
+        else:
+            diff_x = neighbour_vector.x
+            corr_x = 1
+
+        if neighbour_vector.y == 0:
+            diff_y = 0.1
+            corr_y = 0
+        else:
+            diff_y = neighbour_vector.y
+            corr_y = 1  
+
+        Fc = geo.Vector(-(1/diff_x*corr_x), -(1/diff_y*corr_y))
+        Fe = geo.Vector((1/diff_x*corr_x), (1/diff_y*corr_y))
+        return Fc,Fe
+
+class Boundary():
+    def __init__(self, type, value) -> None:
+        self.type = type
+        self.value = value
